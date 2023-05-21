459a040d089662dd26e0ffa76a75ceabc484f88e
Maria <maria.georgiana4@yahoo.com>
2023-05-21 23:42:14 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,77 @@
+"use strict";
+const grid = document.querySelector('.grid');
+const cells = Array.from(document.querySelectorAll('.cell'));
+let betAmount;
+let spinsRemaining = 4;
+let matchedMultipliers = [];
+function revealMultiplier(spunNumber) {
+    const multiplier = getMultiplier();
+    cells[spunNumber - 1].textContent = `x${multiplier}`;
+}
+function revealAllMultipliers() {
+    cells.forEach((cell, index) => {
+        if (cell.textContent === '') {
+            const multiplier = getMultiplier();
+            cell.textContent = `x${multiplier}`;
+        }
+    });
+}
+function handleKeyPress(event) {
+    if (event.key === ' ') {
+        if (spinsRemaining > 0) {
+            const spunNumber = Math.floor(Math.random() * 9) + 1;
+            console.log(`Spinning... The result is ${spunNumber}!`);
+            revealMultiplier(spunNumber);
+            checkWinCondition(spunNumber);
+            spinsRemaining--;
+            console.log(`Spins remaining: ${spinsRemaining}`);
+            if (spinsRemaining === 0) {
+                revealAllMultipliers();
+                endGame();
+            }
+        }
+    }
+}
+function getMultiplier() {
+    const availableMultipliers = [15, 16, 17, 18, 19, 20];
+    const randomIndex = Math.floor(Math.random() * availableMultipliers.length);
+    return availableMultipliers.splice(randomIndex, 1)[0];
+}
+function checkWinCondition(spunNumber) {
+    const multiplier = parseFloat(cells[spunNumber - 1].textContent.substring(1));
+    if (matchedMultipliers.includes(multiplier)) {
+        console.log(`Matched multiplier: x${multiplier}`);
+    }
+    else {
+        matchedMultipliers.push(multiplier);
+    }
+}
+function endGame() {
+    let maxMultiplier = 0;
+    if (matchedMultipliers.length >= 2) {
+        matchedMultipliers.forEach(multiplier => {
+            if (multiplier > maxMultiplier) {
+                maxMultiplier = multiplier;
+            }
+        });
+        const totalWinAmount = betAmount * maxMultiplier;
+        console.log(`Congratulations! You won ${totalWinAmount} units (bet amount: ${betAmount} x matched multiplier: ${maxMultiplier}).`);
+    }
+    else {
+        console.log('Game over. You did not match enough multipliers to win.');
+    }
+    document.removeEventListener('keypress', handleKeyPress);
+}
+function startGame() {
+    const betAmountInput = prompt("Please enter your bet amount:");
+    if (betAmountInput !== null) {
+        betAmount = parseFloat(betAmountInput);
+        console.log(`Bet Amount: ${betAmount}`);
+        console.log('Press [SPACE] to spin.');
+        document.addEventListener('keypress', handleKeyPress);
+    }
+    else {
+        console.log('Invalid bet amount. Please refresh the page to start again.');
+    }
+}
+startGame();
---
+++ src/index.ts
@@ -0,0 +1,110 @@
+const grid = document.querySelector('.grid');
+const cells = Array.from(document.querySelectorAll('.cell'));
+
+let betAmount: number;
+let spinsRemaining = 4;
+let matchedMultipliers: number[] = [];
+
+// Function to reveal the multiplier in the safe
+function revealMultiplier(spunNumber: number) {
+    const multiplier = getMultiplier();
+    cells[spunNumber - 1].textContent = `x${multiplier}`;
+  }
+  
+  // Function to reveal all multipliers
+  function revealAllMultipliers() {
+    cells.forEach((cell, index) => {
+      if (cell.textContent === '') {
+        const multiplier = getMultiplier();
+        cell.textContent = `x${multiplier}`;
+      }
+    });
+  }
+// Function to handle key press
+function handleKeyPress(event: KeyboardEvent) {
+  if (event.key === ' ') {
+    if (spinsRemaining > 0) {
+      const spunNumber = Math.floor(Math.random() * 9) + 1;
+      console.log(`Spinning... The result is ${spunNumber}!`);
+      revealMultiplier(spunNumber);
+      checkWinCondition(spunNumber);
+      spinsRemaining--;
+      console.log(`Spins remaining: ${spinsRemaining}`);
+      if (spinsRemaining === 0) {
+        revealAllMultipliers();
+        endGame();
+      }
+    }
+  }
+}
+  
+// Function to get a random multiplier
+function getMultiplier(): number {
+  const availableMultipliers = [15, 16, 17, 18, 19, 20];
+  const randomIndex = Math.floor(Math.random() * availableMultipliers.length);
+  return availableMultipliers.splice(randomIndex, 1)[0];
+}
+
+// Function to check the win condition
+function checkWinCondition(spunNumber: number) {
+  const multiplier = parseFloat(cells[spunNumber - 1].textContent!.substring(1));
+  if (matchedMultipliers.includes(multiplier)) {
+    console.log(`Matched multiplier: x${multiplier}`);
+  } else {
+    matchedMultipliers.push(multiplier);
+  }
+}
+
+// Function to end the game
+function endGame() {
+    let maxMultiplier = 0;
+    if (matchedMultipliers.length >= 2) {
+      matchedMultipliers.forEach(multiplier => {
+        if (multiplier > maxMultiplier) {
+          maxMultiplier = multiplier;
+        }
+      });
+      const totalWinAmount = betAmount * maxMultiplier;
+      console.log(`Congratulations! You won ${totalWinAmount} units (bet amount: ${betAmount} x matched multiplier: ${maxMultiplier}).`);
+    } else {
+      console.log('Game over. You did not match enough multipliers to win.');
+    }
+    document.removeEventListener('keypress', handleKeyPress);
+  }
+  
+function startGame() {
+    const betAmountInput = prompt("Please enter your bet amount:");
+    if (betAmountInput !== null) {
+      betAmount = parseFloat(betAmountInput);
+      console.log(`Bet Amount: ${betAmount}`);
+      console.log('Press [SPACE] to spin.');
+      document.addEventListener('keypress', handleKeyPress);
+    } else {
+      console.log('Invalid bet amount. Please refresh the page to start again.');
+    }
+  }
+startGame();  
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+          
